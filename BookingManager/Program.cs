3c2479833cf72b4777a8c126e44613d9fa45cef2
Khun <lykhun@gmail.com>
2025-02-11 13:23:25 +0100

    threading

--- BookingManager/Program.cs
+++ BookingManager/Program.cs
@@ -1,11 +1,27 @@
-﻿using BookingManager.DAL;
-using BookingManager.DAL.Entities;
-using Microsoft.EntityFrameworkCore;
+﻿CancellationTokenSource tokenSrc = new ();
+// démarre une action en arrière plan
+Task t = Task.Run(() =>
+{
+    // s'execute sur un thread différent du thread principal
+    for (int i = 0; i < 10000000; i++)
+    {
+        Console.WriteLine(i);
+        if(tokenSrc.Token.IsCancellationRequested)
+        {
+            break;
+        }
+    }
+}, tokenSrc.Token);
 
-using HotelContext ctx = new HotelContext();
+ConsoleKey key = Console.ReadKey().Key;
+if(key == ConsoleKey.Spacebar)
+{
+    tokenSrc.Cancel();
+}
 
-// Permet de ne pas tracker les éléments chargés
-// améliore les performances
-Customer customer = ctx.Customers.AsNoTracking().ToList()[3];
-customer.LastName = "Person";
-ctx.SaveChanges();
+//await t;
+// s'execute sur le thread principal
+for (int i = 1000000; i < 20000000; i++)
+{
+    Console.WriteLine(i);
+}
