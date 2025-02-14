5d190a12ba5ae515e1a7004cdeadedd24f7a460e
Khun <lykhun@gmail.com>
2025-02-14 14:57:53 +0100

    formulaire

--- BookingManager.MVC/Controllers/TestController.cs
+++ BookingManager.MVC/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookingManager.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookingManager.MVC.Controllers
 {
@@ -7,6 +8,37 @@ namespace BookingManager.MVC.Controllers
         public string Hello([FromQuery] string nom)
         {
             return $"Hello {nom} !!";
+        }
+
+        // [Route(@"[controller]/toto/{heure}")]
+        public ViewResult Pause([FromRoute]int id)
+        {
+            int minutesRestantes =
+                (id - DateTime.Now.Hour - 1) * 60 
+                + (60 - DateTime.Now.Minute); 
+            return View(minutesRestantes);
+        }
+
+        // méthode pour afficher le formulaire
+        public ViewResult Contact()
+        {
+            return View();
+        }
+
+        // méthode pour traiter le formulaire
+        [HttpPost]
+        public IActionResult Contact([FromForm] ContactFormViewModel form)
+        {
+            if(ModelState.IsValid)
+            {
+                // faire un traitement pour envoyer un email
+
+
+                // redirection
+                return RedirectToAction("Index", "Home");
+            }
+            // sinon revenir à la vue
+            return View();
         }
     }
 }
---
+++ BookingManager.MVC/Models/ContactFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingManager.MVC.Models
+{
+    public class ContactFormViewModel
+    {
+        [Required(ErrorMessage = "Ce champs est requis")]
+        [MaxLength(50, ErrorMessage = "C'est trop long")]
+        public string Sujet { get; set; } = null!;
+
+        [Required(ErrorMessage = "Ce champs est requis")]
+        [MinLength(10, ErrorMessage = "C'est trop court")]
+        public string Message { get; set; } = null!;
+    }
+}
