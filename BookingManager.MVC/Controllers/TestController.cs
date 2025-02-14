e82f2fec9d537d87bc951803c3112ef99bec6f55
Khun <lykhun@gmail.com>
2025-02-14 09:56:38 +0100

    ASP - Init

---
+++ BookingManager.MVC/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingManager.MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Hello([FromQuery] string nom)
+        {
+            return $"Hello {nom} !!";
+        }
+    }
+}
--- BookingManager/Program.cs
+++ BookingManager/Program.cs
@@ -5,7 +5,7 @@ using BookingManager.DAL.Repositories;
 ICustomerRepository customerRepository = new CustomerRepository();
 IOptionRepository optionRepository = new OptionRepository();
 
-Option? o = optionRepository.GetById(4);
+Option o = optionRepository.GetById(4) ?? throw new Exception();
 optionRepository.Remove(o);
 
 
